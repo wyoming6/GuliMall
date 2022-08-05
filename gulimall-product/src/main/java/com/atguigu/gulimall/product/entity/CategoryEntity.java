b18dc4029e8fc9b36802cf6a3d51afdd078c9bf9
wyoming6 <catherine.sun20@gmail.com>
2022-08-05 11:50:36 -0400

    Add product tree structure in Merchandising Service. P45

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/CategoryEntity.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/CategoryEntity.java
@@ -1,10 +1,13 @@
 package com.atguigu.gulimall.product.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
+
 import lombok.Data;
 
 /**
@@ -56,5 +59,11 @@ public class CategoryEntity implements Serializable {
 	 * 
 	 */
 	private Integer productCount;
+
+	/**
+	 * subcategory
+	 */
+	@TableField(exist = false)//This field does not exist in database table
+	private List<CategoryEntity> children;
 
 }
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +21,8 @@ import com.atguigu.gulimall.product.service.CategoryService;
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
 
+//    @Autowired
+//    CategoryDao categoryDao;
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
@@ -24,6 +31,50 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //In fact, baseMapper is of type CategoryDao
+        //1. Find all data
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        //2. Organize data in a 3-level tree structure
+
+        //2.1 Find the first layer of tree
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0
+        ).map((menu)->{
+            menu.setChildren(getChildrens(menu,entities));
+            return menu;
+        }).sorted((menu1,menu2)->{
+            //meaning "menu1.getSort() - menu2.getSort();", but we won't be worried about NullPointerException
+            return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+
+
+
+
+        return level1Menus;
+    }
+
+    //Find all children menus of a given node
+    private List<CategoryEntity> getChildrens(CategoryEntity root,List<CategoryEntity> all){
+
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == root.getCatId();
+        }).map(categoryEntity -> {
+            //Find children menus
+            categoryEntity.setChildren(getChildrens(categoryEntity,all));
+            return categoryEntity;
+        }).sorted((menu1,menu2)->{
+            //Sort children menus
+            //meaning "menu1.getSort() - menu2.getSort();", but we won't be worried about NullPointerException
+            return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
+
+        }).collect(Collectors.toList());
+
+        return children;
     }
 
 }
