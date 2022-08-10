8a3260b894e1a361e16446f2b25b1c23c48ffc5d
wyoming6 <catherine.sun20@gmail.com>
2022-08-10 10:33:36 -0400

    Refine append function. p52

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,34 +1,50 @@
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    :expand-on-click-node="false"
-    show-checkbox
-    node-key="catId"
-    :default-expanded-keys="expandedKey"
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button
-          v-if="node.level <= 2"
-          type="text"
-          size="mini"
-          @click="() => append(data)"
-        >
-          Append
-        </el-button>
-        <el-button
-          v-if="node.childNodes.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >
-          Delete
-        </el-button>
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+        </span>
+      </span> </el-tree
+    >,
+
+    <el-dialog title="Tips" :visible.sync="dialogVisible" width="30%">
+      <!--Form-->
+      <el-form :model="category">
+        <el-form-item label="Category">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="addCategory">Confirm</el-button>
       </span>
-    </span>
-  </el-tree>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -42,7 +58,10 @@ export default {
         children: "children",
         label: "name"
       },
-      expandedKey:[]
+      expandedKey: [],
+      dialogVisible: false,
+      //category的键和CategoryEntity中的成员变量名一致
+      category: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 }
     };
   },
   methods: {
@@ -58,6 +77,30 @@ export default {
 
     append(data) {
       console.log("append", data);
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1; //in case catLevel is string, we convert it to int by *1
+    },
+
+    addCategory() {
+      console.log("提交的三级分类的数据：", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false)
+      }).then(({ data }) => {
+        //pop up a message if operation is successful
+        this.$message({
+          message: "Category saved.",
+          type: "success"
+        });
+        //Close the dialogbox
+        this.dialogVisible = false;
+        //refresh webpage after removing items
+        this.getMenus();
+        //expand parent node after refreshing the page
+        this.expandedKey = [this.category.parentCid];
+      });
     },
 
     remove(node, data) {
@@ -85,9 +128,9 @@ export default {
               type: "success"
             });
             //refresh webpage after removing items
-            this.getMenus(); 
+            this.getMenus();
             //expand parent node after refreshing the page
-            this.expandedKey = [node.parent.data.catId]
+            this.expandedKey = [node.parent.data.catId];
           });
         })
         .catch(() => {});
