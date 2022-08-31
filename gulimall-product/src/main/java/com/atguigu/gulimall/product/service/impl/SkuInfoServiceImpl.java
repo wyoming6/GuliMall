bdde4240ac8fc11b395921ffb8d5717eda20b483
wyoming6 <catherine.sun20@gmail.com>
2022-08-31 14:36:37 -0400

    Add "SKU search" function to Product System/Product Management. p94

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import com.alibaba.nacos.client.utils.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -29,6 +32,45 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
     @Override
     public void saveSkuInfo(SkuInfoEntity skuInfoEntity) {
         this.baseMapper.insert(skuInfoEntity);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> queryWrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.and(wrpper->{
+                wrpper.eq("sku_id", key).or().like("sku_name", key);
+            });
+        }
+
+        String catelogId = (String) params.get("catelogId");
+        if (!StringUtils.isEmpty(catelogId) && !"0".equals(catelogId)) {
+            queryWrapper.eq("catalog_id", catelogId);
+        }
+
+        String brandId = (String) params.get("brandId");
+        if (!StringUtils.isEmpty(brandId) && !"0".equals(brandId)) {
+            queryWrapper.eq("brand_id", brandId);
+        }
+
+        String min = (String) params.get("min");
+        if (!StringUtils.isEmpty(min)) {
+//            BigDecimal minDecimal = new BigDecimal(min);
+//            if (minDecimal.compareTo(new BigDecimal(0))==1)
+//                queryWrapper.ge("price", min);
+            queryWrapper.ge("price", min);
+        }
+
+        String max = (String) params.get("max");
+        if (!StringUtils.isEmpty(max)) {
+            BigDecimal maxDecimal = new BigDecimal(max);
+            if (maxDecimal.compareTo(new BigDecimal(0))==1)
+                queryWrapper.le("price", maxDecimal);
+        }
+
+        IPage<SkuInfoEntity> page = this.page(new Query<SkuInfoEntity>().getPage(params), queryWrapper);
+        return new PageUtils(page);
     }
 
 }
