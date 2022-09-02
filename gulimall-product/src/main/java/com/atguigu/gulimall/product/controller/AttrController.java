07fc5760f69da6bdb9a42792bf115413d5348381
wyoming6 <catherine.sun20@gmail.com>
2022-09-02 12:00:40 -0400

    Add "Update SPU details" to Product Management/Product Maintenance. p100

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/AttrController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/AttrController.java
@@ -1,8 +1,10 @@
 package com.atguigu.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
+import com.atguigu.gulimall.product.entity.ProductAttrValueEntity;
 import com.atguigu.gulimall.product.service.ProductAttrValueService;
 //import com.atguigu.gulimall.product.vo.AttrGroupRelationVo;
 //import com.atguigu.gulimall.product.vo.AttrRespVo;
@@ -24,18 +26,18 @@ public class AttrController {
 
     @Autowired
     ProductAttrValueService productAttrValueService;
-//
-//    ///product/attr/info/{attrId}
-//
-//    // /product/attr/base/listforspu/{spuId}
-//    @GetMapping("/base/listforspu/{spuId}")
-//    public R baseAttrlistforspu(@PathVariable("spuId") Long spuId){
-//
-//        List<ProductAttrValueEntity> entities = productAttrValueService.baseAttrlistforspu(spuId);
-//
-//        return R.ok().put("data",entities);
-//    }
-//
+
+
+
+    // /product/attr/base/listforspu/{spuId}
+    @GetMapping("/base/listforspu/{spuId}")
+    public R baseAttrlistforspu(@PathVariable("spuId") Long spuId){
+
+        List<ProductAttrValueEntity> entities = productAttrValueService.baseAttrlistforspu(spuId);
+
+        return R.ok().put("data",entities);
+    }
+
 
     // /product/attr/base/list/{catelogId} 显示基本属性
     // /product/attr/sale/list/{catelogId} 显示销售属性
@@ -107,14 +109,14 @@ public class AttrController {
     }
 
     ///product/attr/update/{spuId}
-//    @PostMapping("/update/{spuId}")
-//    public R updateSpuAttr(@PathVariable("spuId") Long spuId,
-//                           @RequestBody List<ProductAttrValueEntity> entities){
-//
-//        productAttrValueService.updateSpuAttr(spuId,entities);
-//
-//        return R.ok();
-//    }
+    @PostMapping("/update/{spuId}")
+    public R updateSpuAttr(@PathVariable("spuId") Long spuId,
+                           @RequestBody List<ProductAttrValueEntity> entities){
+
+        productAttrValueService.updateSpuAttr(spuId,entities);
+
+        return R.ok();
+    }
 
     /**
      * 删除
